0f55a4725cbf910ba86524a00f5e336a63ca6bbb
Naeferith <toto2020@hotmail.fr>
2018-10-10 19:19:04 +0200

    Fix Rotate method

--- Client/Shape.cpp
+++ Client/Shape.cpp
@@ -18,21 +18,23 @@ void Shape::setVertices(const vector<Vector2D>& v) { vertices = v; }
 
 void Shape::Translate(const Vector2D V) {
 	for (auto &vertice : vertices) {
-		vertice.setX(vertice.getX() + V.getX());
-		vertice.setY(vertice.getY() + V.getY());
+		vertice.setX(std::round( vertice.getX() + V.getX() ));
+		vertice.setY(std::round( vertice.getY() + V.getY() ));
 	}
 }
 
 void Shape::Scale(const Vector2D point, const double ratio) {
 	for (auto &vertice : vertices) {
-		vertice.setX((vertice.getX() - point.getX()) * ratio);
-		vertice.setY((vertice.getY() - point.getY()) * ratio);
+		vertice.setX(std::round( (vertice.getX() - point.getX()) * ratio ));
+		vertice.setY(std::round( (vertice.getY() - point.getY()) * ratio ));
 	}
 }
 
 void Shape::Rotate(const Vector2D point, const double rad) {
 	for (auto &vertice : vertices) {
-		vertice.setX((vertice.getX() - point.getX()) * cos(rad) - (vertice.getY() - point.getY()) * sin(rad));
-		vertice.setY((vertice.getX() - point.getX()) * sin(rad) + (vertice.getY() - point.getY()) * cos(rad));
+		double newX = std::round((vertice.getX() - point.getX()) * cos(rad) - (vertice.getY() - point.getY()) * sin(rad));
+		double newY = std::round((vertice.getX() - point.getX()) * sin(rad) + (vertice.getY() - point.getY()) * cos(rad));
+		vertice.setX(newX);
+		vertice.setY(newY);
 	}
 }
