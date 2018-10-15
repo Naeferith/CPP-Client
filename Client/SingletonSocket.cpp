5be22c58385862e31e7983e2285580f01120d4bd
Naeferith <toto2020@hotmail.fr>
2018-10-15 12:29:44 +0200

    Exception handeling

--- Client/SingletonSocket.cpp
+++ Client/SingletonSocket.cpp
@@ -4,6 +4,12 @@
 //Aucune instance n'est actuellement créée
 bool SingletonSocket::isCreated = false;
 
+//Winsock n'est pas encore initialisé
+bool SingletonSocket::isInitialized = false;
+
+//Aucune connexion n'est encore établie
+bool SingletonSocket::isConnected = false;
+
 //Il faut déclarer instance dans le .cpp
 SingletonSocket SingletonSocket::instance;
 
@@ -11,27 +17,23 @@ SingletonSocket::SingletonSocket() {
 	//Empeche une 2eme instance du singleton.
 	if (isCreated) throw logic_error("Vous ne pouvez pas créer une seconde instance de la classe Singleton.");
 	
-	//Initialisation de la librairie Winsock2
-	int r;
-	WSADATA wsaData;
-	r = WSAStartup(MAKEWORD(2, 0), &wsaData);
-	if (r) throw - 1;
-
+	//Empèche une double initialisation
+	if (!isInitialized) {
+		try {
+			InitWS2();
+		}
+		catch (const Erreur& e) {
+			cout << "Erreur : " << e.what() << endl;
+			throw Erreur(-1, "Abort socket construction");
+		}
+		isInitialized = true;
+	}
+	
 	//Initialisation du socket
 	sock = socket(familleAdresses, typeSocket, protocole);
+	if (sock == INVALID_SOCKET)	throw Erreur(WSAGetLastError(), "Failed initialize socket");
 
-	if (sock == INVALID_SOCKET)
-	{
-		ostringstream oss;
-		oss << "la création du socket a échoué : code d'erreur = " << WSAGetLastError() << endl;
-
-		// pour les valeurs renvoyées par WSAGetLastError() : 
-		// cf. doc réf winsock
-
-		throw -2;
-	}
-
-	//
+	///Représentant local
 	sockaddr.sin_family = AF_INET;
 
 	//Conversion de adr en wchar_t *
@@ -52,24 +54,19 @@ SingletonSocket::SingletonSocket() {
 	// htons() assure que le port est bien inscrit dans le format du 
 	// réseau (little-endian ou big-endian)
 
-
-	///Connexion au serveur
-	r = connect(sock, (SOCKADDR *)&sockaddr, sizeof(sockaddr));
-
-	// renvoie une valeur non nulle en cas d'échec.
-
-	// Le code d'erreur peut être obtenu par un appel à WSAGetLastError()
-
-	if (r == SOCKET_ERROR) {
-		cout << "Erreur maggle" << endl;
-		throw - 3;
-	}
-
 	//Affectation de la variable singleton
 	instance = *this;
 
 	//La variable est créée
 	isCreated = true;
+}
+
+void SingletonSocket::InitWS2() {
+	//Initialisation de la librairie Winsock2
+	int r;
+	WSADATA wsaData;
+	r = WSAStartup(MAKEWORD(2, 0), &wsaData);
+	if (r) throw Erreur(-1, "Failed intializing Winsock2");
 }
 
 SingletonSocket::~SingletonSocket() {
@@ -79,6 +76,22 @@ SingletonSocket::~SingletonSocket() {
 SingletonSocket& SingletonSocket::getInstance() { 
 	if (!isCreated) SingletonSocket();
 	return instance; 
+}
+
+bool SingletonSocket::IsConnected(){ return isConnected; }
+
+void SingletonSocket::Connect(){
+	if (!isCreated) throw Erreur(-1, "Socket non créé");
+	if (isCreated && !isConnected) {
+		int r = connect(instance.sock, (SOCKADDR *)&instance.sockaddr, sizeof(sockaddr));
+		if (r == SOCKET_ERROR) throw Erreur(WSAGetLastError(), "Connexion au serveur impossible");
+	}
+}
+
+void SingletonSocket::Send(const char * msg, const int length){
+	if (!isConnected) Connect();
+	int r = send(instance.sock, msg, length, 0);
+	if (r == SOCKET_ERROR) throw Erreur(-1, "Erreur lors de l'envoi de message");
 }
 
 const SOCKET SingletonSocket::getSocket() const { return sock; }
