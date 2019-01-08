020a430196d9de22b05643421058a2ae1fd5f75a
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2019-01-08 16:15:51 +0100

    Color on 256

--- Client/Color.cpp
+++ Client/Color.cpp
@@ -2,11 +2,11 @@
 #include "Color.h"
 
 const Color Color::BLACK(0, 0, 0);
-const Color Color::BLUE(0, 0, 1);
-const Color Color::CYAN(0, 1, 1);
-const Color Color::GREEN(0, 1, 0);
-const Color Color::RED(1, 0, 0);
-const Color Color::YELLOW(1, 1, 0);
+const Color Color::BLUE(0, 0, 255);
+const Color Color::CYAN(0, 255, 255);
+const Color Color::GREEN(0, 255, 0);
+const Color Color::RED(255, 0, 0);
+const Color Color::YELLOW(255, 255, 0);
 
 Color::operator string() const {
 	ostringstream oss;
