94814fe63c85a74de3678de77b358638d169d1cf
Naeferith <toto2020@hotmail.fr>
2019-01-14 16:53:32 +0100

    Optimize Scale

--- Client/Circle.cpp
+++ Client/Circle.cpp
@@ -19,6 +19,11 @@ void Circle::setRadius(const int i) { radius = i; }
 
 string Circle::getName() const { return string("circle"); }
 
+void Circle::Scale(const Vector2D & point, const double ratio) {
+	Shape::Scale(point, ratio);
+	radius *= ratio;
+}
+
 string * Circle::accept(Visitor * v) { return v->visit(this); }
 
 Circle::operator string() const {
