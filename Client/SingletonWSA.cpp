0af2360d7d3eaa98303a9f84c83fe1d5f4a67484
Naeferith <toto2020@hotmail.fr>
2019-01-20 17:27:58 +0100

    Fix Typo

--- Client/SingletonWSA.cpp
+++ Client/SingletonWSA.cpp
@@ -11,11 +11,11 @@ SingletonWSA::SingletonWSA() {
 
 	if (WSAStartup(MAKEWORD(2, 0), &wsaData)) throw Erreur(WSAGetLastError(),"Failed initializing Winsock2");
 	/* MAKEWORD(2,0) sert à indiquer la version de la librairie à utiliser : 1 pour winsock et 2 pour winsock2
-	   en cas de succès, wsaData a été initialisée et l'appel a renvoyà la valeur 0. */
+	   en cas de succès, wsaData a été initialisée et l'appel a renvoy à la valeur 0. */
 }
 
 SingletonWSA::~SingletonWSA() {
-	//Si l'instance a bien été crée précédement on libére les librairies init.
+	//Si l'instance a bien été crée précédement on libère les librairies init.
 	if (instance != NULL) { WSACleanup(); instance = NULL; }
 }
 
